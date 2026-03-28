643a6adedd983df51e6f91107cd628226526f369
PECalle <PECalle@baustro.fin.ec>
2026-03-28 09:59:58 -0500

    Refactor side menu structure

---
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOptions {
+  label: string;
+  route: string;
+  sublabel: string;
+  icon: string
+}
+
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './side-menu-options.component.html',
+})
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOptions[] =[
+
+    {
+      label:'Trending',
+      sublabel:'Gifs populares',
+      route:'/dashboard/trending',
+      icon:'fa-solid fa-chart-line'
+    },
+    {
+      label:'Buscador',
+      sublabel:'Buscar gifs',
+      route:'/dashboard/search',
+      icon:'fa-solid fa-magnifying-glass'
+    },
+  ]
+}
