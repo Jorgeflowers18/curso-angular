4e8f55c91c133f29057a4b6b54e2f3fdc0109c95
PECalle <PECalle@baustro.fin.ec>
2026-03-31 18:27:34 -0500

    Add gif list components

---
+++ src/app/components/gif-list/gif-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
+
+@Component({
+  selector: 'gif-list',
+  imports: [GifListItemComponent],
+  templateUrl: './gif-list.component.html',
+})
+export class GifListComponent {
+
+
+  //input: string[]
+  gifs = input.required<string[]>();
+}
